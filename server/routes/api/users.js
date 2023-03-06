c65bfb757532ba0957f6b4056367a5a095393d1a
Rishabh Pal <boletorishabh1999@gmail.com>
2023-03-06 11:09:02 +0530

    first code setup

---
+++ server/routes/api/users.js
@@ -0,0 +1,69 @@
+const express = require("express");
+const router = express.Router();
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const User = require("../../models/User");
+const { JWT_SECRET } = process.env;
+
+// Register a user
+router.post("/register", async (req, res) => {
+    try {
+        const { email, password } = req.body;
+
+        // Check if user already exists
+        const user = await User.findOne({ email });
+        if (user) {
+            return res.status(400).json({ msg: "User already exists" });
+        }
+
+        // Hash the password
+        const hashedPassword = await bcrypt.hash(password, 10);
+
+        // Create a new user
+        const newUser = new User({ email, password: hashedPassword });
+        await newUser.save();
+
+        res.status(201).json({ msg: "User created successfully" });
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({ msg: "Server error" });
+    }
+});
+
+// Login a user
+router.post("/login", async (req, res) => {
+    try {
+        const { email, password } = req.body;
+
+        // Check if user exists
+        const user = await User.findOne({ email });
+        if (!user) {
+            return res.status(400).json({ msg: "Invalid credentials" });
+        }
+
+        // Check if password is correct
+        const isMatch = await bcrypt.compare(password, user.password);
+        if (!isMatch) {
+            return res.status(400).json({ msg: "Invalid credentials" });
+        }
+
+        // Generate JWT token
+        const token = jwt.sign({ userId: user._id }, JWT_SECRET, { expiresIn: "1d" });
+
+        res.status(200).json({ token });
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({ msg: "Server error" });
+    }
+});
+
+router.get('/login', (req, res) => {
+    try {
+        res.status(200).json({ msg: "I have loged in." });
+    } catch (err) {
+        console.log(err);
+        res.status(404).json({ msg: err });
+    }
+})
+
+module.exports = router;
---
+++ server/server.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+require("dotenv").config();
+
+const app = express();
+
+// Middleware
+app.use(bodyParser.json());
+app.use(cors());
+
+// Routes
+app.use("/api/users", require("./routes/api/users"));
+
+// Connect to MongoDB
+// const mongouri = "mongodb + srv://boletorishabh1999:5v2xkelqNtzxigOf@cluster0.eec441u.mongodb.net/?retryWrites=true&w=majority";
+mongoose
+    .connect("mongodb+srv://boletorishabh1999:5v2xkelqNtzxigOf@cluster0.eec441u.mongodb.net/?retryWrites=true&w=majority", {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    })
+    .then(() => console.log("MongoDB connected"))
+    .catch((err) => console.error(err));
+
+// Start server
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
