0eee41afe40c58a60204be3556d85182aad6f0c0
Rishabh Pal <boletorishabh1999@gmail.com>
2023-03-07 14:56:15 +0530

    contact page created

--- client/src/index.js
+++ client/src/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import Router from './routes';
+import Routes from './routes';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <>
-    <Router />
+    <Routes />
   </>
 );
 
