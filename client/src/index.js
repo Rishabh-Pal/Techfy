9f68968b35ad8da72c4063eb331a83c1bba01c03
Rahul Maurya <rahulmaurya1646@gmail.com>
2023-03-09 19:11:09 +0530

    index.js error fix

--- client/src/index.js
+++ client/src/index.js
@@ -1,22 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-<<<<<<< HEAD
-import Routes from './routes';
-
+import App from './App';
+import './index.css';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <>
-    <Routes />
-=======
-//import Router from './routes';
-import App from './App'
-import './index.css'
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <>
-     <App></App>
-    {/* <Router /> */}
->>>>>>> 163eb52da34a1133b7c2dd77923c3690b21840ad
+<>
+    <App />
+ 
   </>
+
 );
 
